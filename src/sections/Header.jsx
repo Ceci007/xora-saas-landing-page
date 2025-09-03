4e5864dbd60c2a11d70fa869deb72bb2ff83d240
UnicornDad8 <cecibenitezca@gmail.com>
2025-09-03 11:43:13 -0300

    Add react-scroll packages and start navbar styles

---
+++ src/sections/Header.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react'
+import { Link as LinkScroll } from "react-scroll"
+
+const NavLink = ({ title }) => (
+  <LinkScroll
+    className="uppercase transition-colors duration-500 cursor-pointer base-bold text-p4 hover:text-p1 max-lg:my-4 max-lg:h5"
+  >
+    {title}
+  </LinkScroll>
+)
+
+const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <header className="fixed top-0 left-0 z-50 w-full py-10">
+      <div className="container flex items-center h-14 max-lg:px-5">
+        <a className="flex-1 cursor-pointer lg:hidden z-2">
+          <img src="/images/xora.svg" width={115} height={55} alt="logo" />
+        </a>
+
+        <div className="w-full max-lg:fixed max-lg:top-0 max-lg:left-0 mx-lg:w-full max-lg:bg-s2 max-lg:opacity-0">
+          <div className="max-lg:relative max-lg:flex max-lg:flex-col max-lg:min-h-screen max-lg:p-6 max-lg:overflow-hidden sidebar-before max-md:px-4">
+            <nav className="max-lg:relative max-lg:z-2 max-lg:my-auto">
+              <ul className="flex max-lg:block max-lg:px-12">
+                <li className="nav-li">
+                  <NavLink title="features" />
+                  <div className="dot" />
+                  <NavLink title="pricing" />
+                </li>
+                <li className="nav-logo">
+                  <LinkScroll>
+                    <img src="/images/xora.svg" width={160} height={55} alt="logo" />
+                  </LinkScroll>
+                </li>
+                <li className="nav-li">
+                  <NavLink title="faq" />
+                  <div className="dot" />
+                  <NavLink title="Download" />
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+        <button className="flex items-center justify-center border-2 rounded-full lg:hidden z-2 size-10 border-s4/25"
+          onClick={() => setIsOpen((prev) => !prev)}
+        > 
+          <img src={`/images/${isOpen ? "close" : "magic"}.svg`} alt="magic" className="object-contain size-1/2" />
+        </button>
+      </div>
+    </header>
+  )
+}
+
+export default Header;
