e6193d4ed7da3ae49e354b4f627d5166401ffa67
UnicornDad8 <cecibenitezca@gmail.com>
2025-09-04 15:25:48 -0300

    Start styling the picing section

---
+++ src/sections/Pricing.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react'
+import { Element } from 'react-scroll'
+import clsx from 'clsx'
+import { plans } from '../constants';
+
+const Pricing = () => {
+  const [monthly, setMonthly] = useState(false);
+
+  return (
+    <section>
+      <Element name="pricing">
+        <div className="container">
+          <div className="relative pb-40 mx-auto border-l border-r max-w-960 pricing-head_before border-s2 bg-s1/50 pt-28 max-xl:max-w-4xl max-lg:border-none max-md:pb-32 max-md:pt-16">
+            <h3 className="relative max-w-lg mx-auto text-center h3 max-lg:h4 max-md:h5 z-3 mb-14 text-p4 max-md:mb-11 max-sm:max-w-sm">
+                Flexible pricing for teams of all sizes
+            </h3>
+            <div className="relative z-4 mx-auto flex w-[375px] rounded-3xl border-[3px] border-s4/25 bg-s1/50 p-2 backdrop-blur-[6px] max-md:w-[310px]">
+              <button className={clsx("pricing-head_btn", monthly && "text-p4")} onClick={() => setMonthly(true)}>Monthly</button>
+              <button className={clsx("pricing-head_btn", !monthly && "text-p4")} onClick={() => setMonthly(false)}>Annual</button>
+              <div className={clsx("g4 rounded-14 before:h-100 pricing-head_btn_before absolute left-2 top-2 h-[calc(100%-16px)] w-[calc(50%-8px)] overflow-hidden shadow-400 transition-transform duration-500", !monthly && "translate-x-full")} />
+            </div>
+            <div className="pricing-bg">
+              <img src="/images/bg-outlines.svg" width={960} height={380} alt="Outline" className="relative z-2" />
+              <img src="/images/bg-outlines-fill.png" width={960} height={380} alt="Outline Fill" className="absolute inset-0 opacity-5 mix-blend-soft-light" />
+            </div>
+          </div>
+
+          <div className="relative flex items-start -mt-12 scroll-hide z-2 max-xl:gap-5 max-xl:overflow-auto max-xl:pt-6">
+            {plans.map((plan, index) => (
+              <div key={plan.id} className="relative border-2 pricing-plan_first pricing-plan_last pricing-plan_odd pricing-plan_even p-7 max-xl:min-w-80 max-lg:rounded-3xl xl:w-[calc(33.33%+2px)]">
+                {index === 1 && (
+                  <div className="absolute top-0 left-0 right-0 rounded-tl-3xl rounded-tr-3xl g4 h-330 z-1" />
+                )}
+                <div className={clsx("absolute left-0 right-0 z-2 flex items-center justify-center", index === 1 ? "-top-6" : "-top6 xl:-top-11")}>
+                  <img src={plan.logo} alt={plan.title} className={clsx("object-contain drop-shadow-2xl", index === 1 ? "size-[120px]" : "size-[88px]")} />
+                </div>
+                <div className={clsx("relative flex flex-col items-center", index === 1 ? "pt-24" : "pt-12")}>
+                  <div className={clsx("small-2 rounded-20 relative z-2 mx-auto mb-6 border-2 px-4 py-1.5 uppercase", index === 1 ? "border-p3 text-p3" : "border-p1 text-p1")}>
+                    {plan.title}
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </Element>
+    </section>
+  )
+}
+
+export default Pricing;
